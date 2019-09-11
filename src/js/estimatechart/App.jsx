4cf6c890379187c5b9d9d5c32c95b4ed03754b64
Tomohiro Hizaki <magatama.club@gmail.com>
2019-09-11 16:13:33 +0900

    グラフいじりまくる

--- src/js/estimatechart/App.jsx
+++ src/js/estimatechart/App.jsx
@@ -1,33 +1,51 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine,
+  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,
 } from 'recharts';
 import budgetRange from './constants/budget_range';
 
 const data = [
   {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    name: 'Page A', value: 0, uv: 4000, pv: 2400, amt: 2400,
   },
   {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    name: 'Page B', value: 10, uv: 3000, pv: 1398, amt: 2210,
   },
   {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    name: 'Page C', value: 20, uv: -1000, pv: 9800, amt: 2290,
   },
   {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    name: 'Page D', value: 30, uv: 500, pv: 3908, amt: 2000,
   },
   {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    name: 'Page E', value: 40, uv: -2000, pv: 4800, amt: 2181,
   },
   {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    name: 'Page F', value: 50, uv: -250, pv: 3800, amt: 2500,
   },
   {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    name: 'Page G', value: 60, uv: 3490, pv: 4300, amt: 2100,
   },
 ];
+
+const gradientOffset = () => {
+  const dataMax = Math.max(...data.map(i => i.uv));
+  const dataMin = Math.min(...data.map(i => i.uv));
+  //   const dataMax = 30;
+  //   const dataMin = 20;
+
+  if (dataMax <= 0) {
+    return 0;
+  }
+  if (dataMin >= 0) {
+    return 1;
+  }
+
+  return dataMax / (dataMax - dataMin);
+};
+
+const off = gradientOffset();
 
 const App = ({ categoryId, privateId, workId }) => {
   console.log(categoryId, privateId, workId);
@@ -45,22 +63,29 @@ const App = ({ categoryId, privateId, workId }) => {
       <span>
         {`${budgetRange.get(value).from}円〜${budgetRange.get(value).to}円`}
       </span>
-      <LineChart
+      <AreaChart
         width={500}
-        height={300}
+        height={400}
         data={data}
         margin={{
-          top: 20, right: 50, left: 20, bottom: 5,
+          top: 10, right: 30, left: 0, bottom: 0,
         }}
       >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
         <Tooltip />
-        <Legend />
-        <ReferenceLine x="Page C" stroke="red" label="Max PV PAGE" />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
+        <XAxis dataKey="value" />
+        <YAxis dataKey="amt" />
+        <defs>
+          <linearGradient id="splitColor" x1="0" y1="0" x2="1" y2="0">
+            <stop offset={0.3} stopColor="blue" stopOpacity={1} />
+            <stop offset={0.3} stopColor="transparent" stopOpacity={1} />
+            <stop offset={0.6} stopColor="transparent" stopOpacity={1} />
+            <stop offset={0.6} stopColor="red" stopOpacity={1} />
+            <stop offset={0.8} stopColor="red" stopOpacity={1} />
+            <stop offset={0.8} stopColor="transparent" stopOpacity={1} />
+          </linearGradient>
+        </defs>
+        <Area type="monotone" dataKey="amt" stroke="#000" fill="url(#splitColor)" />
+      </AreaChart>
     </div>
   );
 };
