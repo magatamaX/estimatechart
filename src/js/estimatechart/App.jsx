fbb4a6f9ff4cb31337359d94a3de9745c5776d48
Tomohiro Hizaki <magatama.club@gmail.com>
2019-09-11 15:13:36 +0900

    prettierっていらない

--- src/js/estimatechart/App.jsx
+++ src/js/estimatechart/App.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import budgetRange from './constants/budget_range';
 
 const App = ({ data }) => {
-    document.getElementById('WorkBudgetFixed').addEventListener('change', e => {
-        console.log('Innner Component => change', e.target.value);
-    });
-    console.log(data);
-    return <p>{data}</p>;
+  console.log(data);
+
+  const [value, setValue] = useState(1);
+  console.log(budgetRange.get(value));
+
+  document.getElementById('WorkBudgetFixed').addEventListener('change', (e) => {
+    console.log('Innner Component => change', e.target.value);
+    setValue(Number(e.target.value));
+  });
+
+  return (
+    <div>
+      <span>
+        {`${budgetRange.get(value).from}円〜${budgetRange.get(value).to}円`}
+      </span>
+    </div>
+  );
 };
 
 App.propTypes = {
-    data: PropTypes.string.isRequired
+  data: PropTypes.string.isRequired,
 };
 
 export default App;
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './estimatechart/App';
 
-document.getElementById('WorkBudgetFixed').addEventListener('change', e => {
-    console.log('change', e.target.value);
+document.getElementById('WorkBudgetFixed').addEventListener('change', (e) => {
+  console.log('change', e.target.value);
 });
 
 ReactDOM.render(<App />, document.querySelector('.js-estimate-chart'));
